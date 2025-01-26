3c495cfb1533cc9dbf04778402bf22d6372b9076
timb <tynrare@gmail.com>
2025-01-26 07:52:02 +0300

    rewipe

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-
 main();
 
-async function main() {
-    const flip = document.querySelector("bb#flip");
-    const source = document.querySelector("db#source");
-    const link = source.innerHTML + `?t=${Math.random()}`;
-    const content = await fetch(link);
-    flip.innerHTML = await content.text();
-
-    fullscreen();
-}
-
-function fullscreen() {
-    if (window.location.hash.includes("fullscreen")) {
-        document.querySelector("#fullscreen")?.classList.add("fullscreen");
-    }
+function main() {
 }
