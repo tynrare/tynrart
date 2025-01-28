db4a522f8c2fef2d67269b5274172bcb477d87e2
tynroar <tynrare@gmail.com>
2025-01-29 02:31:23 +0300

    bline Bresenham's line algorithm

--- index.js
+++ index.js
@@ -43,8 +43,18 @@ function App(config) {
 		ctx.drawImage(images[imgname], x * CELLSIZE, y * CELLSIZE, CELLSIZE, CELLSIZE);
 	}
 	
-	function dline(ax, ay, bx, by) {
-	
+	function bline(x0, y0, x1, y1) {
+		var dx = Math.abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+		var dy = Math.abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+		var err = (dx>dy ? dx : -dy)/2;
+
+		while (true) {
+			draw("tile", x0,y0);
+			if (x0 === x1 && y0 === y1) break;
+			var e2 = err;
+			if (e2 > -dx) { err -= dy; x0 += sx; }
+			if (e2 < dy) { err += dx; y0 += sy; }
+		}
 	}
 	
 	// --- game
@@ -58,6 +68,8 @@ function App(config) {
 		const imgkey = images_keys[im % images_len];
 
 		draw(imgkey, x, y);
+
+		bline(4, 4, 7, 10);
 	}
 	
 	// --- tek
